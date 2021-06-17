653a2dbf0904ecadf715884fa6df156ee9ae22cc
RYANRANAUT <ryanranaut@gmail.com>
2021-06-18 02:16:04 +0530

    init commit

---
+++ tictactoe/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>This is Ryan Ranaut</p>
+    </>
+  );
+};
+
+export default App;
